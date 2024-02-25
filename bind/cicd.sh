fe4484ac33ea3b3327e67495b55fe43cd1580a7d
dmotte <37443982+dmotte@users.noreply.github.com>
2024-02-25 14:43:16 +0100

    CICD_VERSION_EXPR with CICD_GIT_REF pre-evaluation

--- bind/cicd.sh
+++ bind/cicd.sh
@@ -8,5 +8,5 @@ IMG_AUTHOR=dmotte IMG_NAME=bind \
 IMG_DESCRIPTION='BIND DNS server on alpine' \
 IMG_FULL_DESCRIPTION_FILE=../dockerhub-description.md \
 IMG_PLATFORMS=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
-CICD_VERSION_EXPR='version_by_expr $CICD_GIT_REF '\''echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f2)"'\' \
+CICD_VERSION_EXPR="version_by_expr ${CICD_GIT_REF@Q} "\''echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f2)"'\' \
 bash "$CICD_SCRIPTS_DIR/docker-img.sh"
--- dnsmasq/cicd.sh
+++ dnsmasq/cicd.sh
@@ -8,5 +8,5 @@ IMG_AUTHOR=dmotte IMG_NAME=dnsmasq \
 IMG_DESCRIPTION='dnsmasq on alpine' \
 IMG_FULL_DESCRIPTION_FILE=../dockerhub-description.md \
 IMG_PLATFORMS=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
-CICD_VERSION_EXPR='version_by_expr $CICD_GIT_REF '\''echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f3)"'\' \
+CICD_VERSION_EXPR="version_by_expr ${CICD_GIT_REF@Q} "\''echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f3)"'\' \
 bash "$CICD_SCRIPTS_DIR/docker-img.sh"
