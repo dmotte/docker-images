8360c2f6f34ceb1f4f1d5b5e038515bc0306e28c
dmotte <37443982+dmotte@users.noreply.github.com>
2024-03-07 00:18:54 +0100

    Extracted version sub expression to separate variable

--- bind/cicd.sh
+++ bind/cicd.sh
@@ -4,9 +4,12 @@ set -e
 
 cd "$(dirname "$0")"
 
+# shellcheck disable=SC2016
+sub_expr='echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f2)"'
+
 IMG_AUTHOR=dmotte IMG_NAME=bind \
 IMG_DESCRIPTION='BIND DNS server on alpine' \
 IMG_FULL_DESCRIPTION_FILE=../dockerhub-description.md \
 IMG_PLATFORMS=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
-CICD_VERSION_EXPR="version_by_expr ${CICD_GIT_REF@Q} "\''echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f2)"'\' \
+CICD_VERSION_EXPR="version_by_expr ${CICD_GIT_REF@Q} ${sub_expr@Q}" \
 bash "$CICD_SCRIPTS_DIR/docker-img.sh"
--- socat/cicd.sh
+++ socat/cicd.sh
@@ -4,9 +4,12 @@ set -e
 
 cd "$(dirname "$0")"
 
+# shellcheck disable=SC2016
+sub_expr='echo "v$(docker run --rm "$TMP_IMG" -V | head -2 | tail -1 | cut -d" " -f3)"'
+
 IMG_AUTHOR=dmotte IMG_NAME=socat \
 IMG_DESCRIPTION='socat on alpine' \
 IMG_FULL_DESCRIPTION_FILE=../dockerhub-description.md \
 IMG_PLATFORMS=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64 \
-CICD_VERSION_EXPR="version_by_expr ${CICD_GIT_REF@Q} "\''echo "v$(docker run --rm "$TMP_IMG" -V | head -2 | tail -1 | cut -d" " -f3)"'\' \
+CICD_VERSION_EXPR="version_by_expr ${CICD_GIT_REF@Q} ${sub_expr@Q}" \
 bash "$CICD_SCRIPTS_DIR/docker-img.sh"
