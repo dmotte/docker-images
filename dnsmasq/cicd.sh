3c4ad943ede65f294051c015d27c446905415d76
dmotte <37443982+dmotte@users.noreply.github.com>
2024-06-28 02:48:52 +0200

    Lowercase tmp_img

--- dnsmasq/cicd.sh
+++ dnsmasq/cicd.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname "$0")"
 
 # shellcheck disable=SC2016
-sub_expr='echo "v$(docker run --rm "$TMP_IMG" -v | head -1 | cut -d" " -f3)"'
+sub_expr='echo "v$(docker run --rm "$tmp_img" -v | head -1 | cut -d" " -f3)"'
 
 IMG_AUTHOR=dmotte IMG_NAME=dnsmasq \
 IMG_DESCRIPTION='dnsmasq on alpine' \
